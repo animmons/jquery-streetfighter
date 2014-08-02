a6ea96d3f212badc0ddfe70b4daa234cd8f724ee
Alexandra Nimmons <you@example.com>
2014-08-02 19:21:58 -0400

    third

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     	$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
+
   	.mousedown(function() {
   		playHadouken();    
   			$('.ryu-ready').hide();
@@ -26,12 +27,44 @@ $(document).ready(function() {
   		$('.ryu-ready').show();
 	});
 
+
+
+document.onkeydown = function(e){
+    e = e || window.event;
+    var key = e.which || e.keyCode;
+    if(key===88){
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+    } 
+}
+
+
+document.onkeyup = function(e){
+    e = e || window.event;
+    var key = e.which || e.keyCode;
+    if(key===88){
+        $('.ryu-still').show();
+        $('.ryu-ready').show();
+        $('.ryu-cool').hide();
+    } 
+}
+
+
+//http://stackoverflow.com/questions/14261062/js-function-when-keyboard-key-is-pressed
+
 })
+
+
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+
+
 
 
